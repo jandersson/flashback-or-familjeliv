43c5f3ec0a613b0a0c37f0a416c008be3a0b2b69
jandersson <jonas@andersson.io>
2016-12-07 22:14:32 +0100

    add support for getting tagged words, lemmas, and sentences

--- src/forum_corpus_reader.py
+++ src/forum_corpus_reader.py
@@ -4,10 +4,50 @@ from nltk.corpus.reader.xmldocs import XMLCorpusReader
 # Prototyped in notebook 0.1
 class ForumCorpusReader(XMLCorpusReader):
     def __init__(self, root, fileid):
+        self.path = root+fileid
         XMLCorpusReader.__init__(self, root, fileid)
 
-    def sentences(self, fileid):
-        '''This does nothing at the moment, but it would be nice to be able to return the sentences.
-        This is probably possible with the XMLCorpusView, where you can specify tags of interest.
+    def sentences(self):
+        '''Returns a list of sentences where each sentence is a list of words
         '''
-        return XMLCorpusView(fileid, 'sentences')
+        sents = XMLCorpusView(self.path, '.*/sentence')
+        sent_list = list()
+        for sentence in sents:
+            word_list = [word.text for word in sentence]
+            sent_list.append(word_list)
+        return sent_list
+
+    def tagged_words(self, lemmatize=True):
+        words = XMLCorpusView(self.path, '.*/w')
+        if lemmatize:
+            word_tags = [ (word.text,
+                           word.attrib['pos'],
+                           word.attrib['lemma'].replace("|", ""))
+                         for word in words ]
+        else:
+            word_tags = [ (word.text, word.attrib['pos']) for word in words ]
+        return word_tags
+
+    def tagged_sentences(self, lemmatize=True):
+        sents = XMLCorpusView(self.path, '.*/sentence')
+        sent_list = list()
+        if lemmatize:
+            for sent in sents:
+                if lemmatize:
+                    word_list = [ (word.text,
+                                   word.attrib['pos'],
+                                   word.attrib['lemma'].replace("|", ""))
+                                   for word in sent ]
+                else:
+                    word_list = [ (word.text,
+                                   word.attrib['pos'])
+                                   for word in sent ]
+                sent_list.append(word_list)
+        else:
+            sent_list = list()
+            word_list = []
+        return sent_list
+
+if __name__ == '__main__':
+    fam_corpus = ForumCorpusReader('data/', 'familjeliv-sex25.xml')
+    print(fam_corpus.tagged_sentences()[:2])
