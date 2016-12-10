cfbf45c9fa019b992620195795d45fdaffb9fb8e
Samuel Murray <samuel.murray@outlook.com>
2016-12-10 15:06:08 +0100

    Changed to work with profile features

--- src/flashback_or_familjeliv.py
+++ src/flashback_or_familjeliv.py
@@ -1,33 +1,32 @@
 from nltk import classify
 from nltk.classify import NaiveBayesClassifier
+import random
 
 from forum_profile import ForumProfile
 from forum_corpus_reader import ForumCorpusReader
 
 DATA_DIR = 'data/'
-N_GRAMS = 1
+FAM_FILE = r'familjeliv-sex1.xml'
+FLASH_FILE = r'flashback-sex1.xml'
+NGRAM_ORDER = 1
+TRAIN_TEST_RATIO = 0.7
 
 
 def generate_features(file, label):
-    # corpus = ForumCorpusReader(DATA_DIR, file)
-    # profile = ForumProfile(corpus, N_GRAMS)
-    # return profile.features()
-    return [({file: 1}, label) for x in range(10)]
+    corpus = ForumCorpusReader(DATA_DIR, file)
+    profile = ForumProfile(corpus, label, NGRAM_ORDER)
+    return profile.generate_features()
 
 
 def train_and_classify():
-    fam_file = r'familjeliv-sex1.xml'
-    flash_file = r'flashback-sex1.xml'
-
-    fam_set = generate_features(fam_file, 'fam')
-    flash_set = generate_features(flash_file, 'flash')
-
+    fam_set = generate_features(FAM_FILE, 'fam')
+    flash_set = generate_features(FLASH_FILE, 'flash')
     data_set = fam_set + flash_set
-    import random
+
     random.shuffle(data_set)
     data_size = len(data_set)
-    train_set = data_set[:int(0.7 * data_size)]
-    test_set = data_set[int(0.7 * data_size):]
+    train_set = data_set[:int(TRAIN_TEST_RATIO * data_size)]
+    test_set = data_set[int(TRAIN_TEST_RATIO * data_size):]
     test_set_unlabeled = [features for (features, labels) in test_set]
 
     classifier = NaiveBayesClassifier.train(train_set)
